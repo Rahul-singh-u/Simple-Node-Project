1223f10923ca1053ceb82a7e595ace78d268e9a2
rahul <rahulrajsingh666@gmail.com>
2020-07-22 23:43:59 +0530

    Initial Commit By Rahul Singh

---
+++ index.js
@@ -0,0 +1,6 @@
+let a = 5;
+let b = 7;
+let c = a + b;
+
+//console.log(`Tha value of c : ${c}`);
+console.log('Tha value of c : ' +c);
---
+++ test.js
@@ -0,0 +1,6 @@
+let a = 5;
+let b = 9;
+let c = a+b;
+
+
+console.log("the value is :"+c)
