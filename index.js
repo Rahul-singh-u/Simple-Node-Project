53fca9e8874745da6e4d9a85bcca29ce9fa95e66
rahul <rahulrajsingh666@gmail.com>
2020-07-23 00:03:31 +0530

    new code added

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ let a = 5;
 let b = 7;
 let c = a + b;
 let d = a * b;
+let e = a / b;
 
 //Commented by Prashant singh Date:22-07-20
 console.log(`Tha value of c : ${d}`);
+//Commented by Rahul
 
+console.log(`Tha value of c : ${e}`);
 console.log('Tha value of c : ' +c);
